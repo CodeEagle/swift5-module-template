b1cc6894b8f984a24e8182857f0e1c9300b21946
Lincoln <luohuanglue@corp.netease.com>
2021-08-16 16:23:40 +0800

    change: add more customize for project template

---
+++ xxPROJECTxNAMExx/iOS Example/iOS-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Example
+//
+//  Created by __AUTHOR NAME__ on __TODAYS_DATE__.
+//
+
+import UIKit
+import xxPROJECTxNAMExx
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
